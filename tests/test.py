a00f45449e2fd234f23cf8d9a5d70bdc6a8f22c9
Itrat Rahman <rahmanitrat@outlook.com>
2018-06-25 09:22:51 +0600

    created the skeletal of deep_learn package

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+# add the directory of the package to the system path
+from config import *
+append_path('../')
+
+# import statements
+import pandas as pd
+import numpy as np
+import math
+import time
