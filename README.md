1003042adab96c375da1a2b3f059287f0ed6365f
Itrat Rahman <rahmanitrat@outlook.com>
2018-11-28 17:59:49 +0600

    modified directory structure

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Package Description
 
 ## Intro
-deep_learn package contains numpy of implementation of a standard feed forward neural network. The model class `deep_learn.nn.ann` which implements feed forward neural network takes in network architecture as object field. The class method `fit` which trains the model takes train and test data and neural network hyperparameters as arguments. Here every mathematical operation is implemented using numpy to speed up computation. Other than numpy and built-in packages no other specialized libraries are used to build package.
+deep_learn package contains numpy of implementation of a standard feed forward neural network. This package will be used as teaching material for my upcoming deep learning course. The model class `deep_learn.nn.ann` which implements feed forward neural network takes in network architecture as object field. The class method `fit` which trains the model takes train and test data and neural network hyperparameters as arguments. Here every mathematical operation is implemented using numpy to speed up computation. Other than numpy and built-in packages no other specialized libraries are used to build package.
 
 ## Folder Description
 - deep_learn - contains the package which contains the class module of feed forward neural network
 - analysis - contains jupyter notebooks that shows example demonstrations of deep learning tasks using the deep_learn package, it also contains the iris_classification notebook which trains neural network to classify iris data (check the notebook)
-- tests - contains `test.py` which carries out unit tests of some methods of the `deep_learn` package. I put the test files outside of the package because, before building the package I planned to do some unit tests against functions of established libraries without tediously building test cases which is time consuming. For example: testing my implementation of logloss function against logloss function of sklearn. Putting the test files outside the package makes sure we dont have to install sklearn as a requirement to install this package.
 
 ## Installation Procedure
 Download the package. Open the terminal, and change current directory to the directory containing the `setup.py` file. Install the package using the following command:
--- deep_learn/nn/neural_network.py
+++ deep_learn/nn/neural_network.py
@@ -29,12 +29,6 @@ class ann(object):
     def initialize_parameters_deep(layer_dims):
         """
         Arguments:
-        layer_dims -- python array (list) containing the dimensions of each layer in our network
-
-        Returns:
-        parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
-                        Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
-                        bl -- bias vector of shape (layer_dims[l], 1)
         """
 
         np.random.seed(1)
@@ -55,16 +49,7 @@ class ann(object):
     @staticmethod
     def linear_forward(A, W, b):
         """
-        Implement the linear part of a layer's forward propagation.
-
-        Arguments:
-        A -- activations from previous layer (or input data): (size of previous layer, number of examples)
-        W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
-        b -- bias vector, numpy array of shape (size of the current layer, 1)
-
-        Returns:
-        Z -- the input of the activation function, also called pre-activation parameter
-        cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
+        Implement the linear part of a layer's forward propagation
         """
 
         Z = W.dot(A) + b
@@ -79,17 +64,6 @@ class ann(object):
     def linear_activation_forward(A_prev, W, b, activation):
         """
         Implement the forward propagation for the LINEAR->ACTIVATION layer
-
-        Arguments:
-        A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
-        W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
-        b -- bias vector, numpy array of shape (size of the current layer, 1)
-        activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-
-        Returns:
-        A -- the output of the activation function, also called the post-activation value
-        cache -- a python dictionary containing "linear_cache" and "activation_cache";
-                 stored for computing the backward pass efficiently
         """
 
         if activation == "sigmoid":
@@ -112,16 +86,6 @@ class ann(object):
     def L_model_forward(X, parameters):
         """
         Implement forward propagation for L layer model
-
-        Arguments:
-        X -- data, numpy array of shape (input size, number of examples)
-        parameters -- output of initialize_parameters_deep()
-
-        Returns:
-        AL -- last post-activation value
-        caches -- list of caches containing:
-                    every cache of linear_relu_forward() (there are L-1 of them, indexed from 0 to L-2)
-                    the cache of linear_sigmoid_forward() (there is one, indexed L-1)
         """
 
         caches = []
@@ -143,15 +107,6 @@ class ann(object):
     def linear_backward(dZ, cache):
         """
         Implement the linear portion of backward propagation for a single layer (layer l)
-
-        Arguments:
-        dZ -- Gradient of the cost with respect to the linear output (of current layer l)
-        cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
-
-        Returns:
-        dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
-        dW -- Gradient of the cost with respect to W (current layer l), same shape as W
-        db -- Gradient of the cost with respect to b (current layer l), same shape as b
         """
         A_prev, W, b = cache
         m = A_prev.shape[1]
@@ -171,16 +126,6 @@ class ann(object):
     def linear_activation_backward(dA, cache, activation):
         """
         Implement the backward propagation for the LINEAR->ACTIVATION layer.
-
-        Arguments:
-        dA -- post-activation gradient for current layer l
-        cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
-        activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-
-        Returns:
-        dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
-        dW -- Gradient of the cost with respect to W (current layer l), same shape as W
-        db -- Gradient of the cost with respect to b (current layer l), same shape as b
         """
         linear_cache, activation_cache = cache
 
@@ -199,19 +144,6 @@ class ann(object):
     def L_model_backward(AL, Y, caches):
         """
         Implement the backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
-
-        Arguments:
-        AL -- probability vector, output of the forward propagation (L_model_forward())
-        Y -- true "label" vector (containing 0 if non-cat, 1 if cat)
-        caches -- list of caches containing:
-                    every cache of linear_activation_forward() with "relu" (there are (L-1) or them, indexes from 0 to L-2)
-                    the cache of linear_activation_forward() with "sigmoid" (there is one, index L-1)
-
-        Returns:
-        grads -- A dictionary with the gradients
-                 grads["dA" + str(l)] = ...
-                 grads["dW" + str(l)] = ...
-                 grads["db" + str(l)] = ...
         """
         grads = {}
         L = len(caches) # the number of layers
@@ -240,15 +172,6 @@ class ann(object):
     def update_parameters(parameters, grads, learning_rate):
         """
         Update parameters using gradient descent
-
-        Arguments:
-        parameters -- python dictionary containing your parameters
-        grads -- python dictionary containing your gradients, output of L_model_backward
-
-        Returns:
-        parameters -- python dictionary containing your updated parameters
-                      parameters["W" + str(l)] = ...
-                      parameters["b" + str(l)] = ...
         """
 
         L = len(parameters) // 2 # number of layers in the neural network
@@ -293,13 +216,6 @@ class ann(object):
     def predict(X, parameters):
         """
         This function is used to predict the results of a  L-layer neural network.
-
-        Arguments:
-        X -- data set of examples you would like to label
-        parameters -- parameters of the trained model
-
-        Returns:
-        p -- predictions for the given dataset X
         """
 
         # Forward propagation
